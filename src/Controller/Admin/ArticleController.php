92267d57616ed07f6b66461883244a0465dc90ec
Шиленко Михаил <shilenkomp@mail.ru>
2021-06-03 11:24:45 +0000

    Модуль 11: "Регистрация и классы Voter" - Домашняя работа

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -2,12 +2,29 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-/**
- * @IsGranted("ROLE_ADMIN_ARTICLE")
- */
 class ArticleController extends AbstractController
 {
+    /**
+     * @Route("/admin/articles/create", name="app_admin_articles_create")
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
+     */
+    public function create()
+    {
+        return new Response("Страница создания  статьи");
+    }
+
+    /**
+     * @Route("/admin/articles/{id}/edit", name="app_admin_articles_edit")
+     * @IsGranted("MANAGE", subject="article")
+     */
+    public function edit(Article $article)
+    {
+        return new Response("Страница редактирования статьи {$article->getTitle()}");
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -55,9 +55,15 @@ class User implements UserInterface
      */
     private $apiTokens;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Article::class, mappedBy="author")
+     */
+    private $articles;
+
     public function __construct()
     {
         $this->apiTokens = new ArrayCollection();
+        $this->articles = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -106,7 +112,8 @@ class User implements UserInterface
         return $this;
     }
 
-    public function hasRole(string $role): bool {
+    public function hasRole(string $role): bool
+    {
         return in_array($role, $this->getRoles());
     }
 
@@ -191,5 +198,35 @@ class User implements UserInterface
     public function getApiTokens(): Collection
     {
         return $this->apiTokens;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->removeElement($article)) {
+            // set the owning side to null (unless already changed)
+            if ($article->getAuthor() === $this) {
+                $article->setAuthor(null);
+            }
+        }
+
+        return $this;
     }
 }
